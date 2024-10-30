02c79835261a20fb5b38ce2acd84bcf914ec2ddc
Zoellisam <zoellisam@gmail.com>
2024-10-30 15:58:05 +0100

    led und disco

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 basic.forever(function () {
+    basic.setLedColor(0x00ffff)
+    if (calliBot2.readBumperSensor(C2Sensor.rechts, C2State.an) || calliBot2.readBumperSensor(C2Sensor.links, C2State.an)) {
+        calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
+        for (let index = 0; index < 2; index++) {
+            calliBot2.motor(C2Motor.beide, C2Dir.rueckwaerts, 50)
+        }
+    }
     if (input.buttonIsPressed(Button.B)) {
         basic.showString("Hi! Ich bin Paul.")
     }
@@ -15,16 +22,38 @@ basic.forever(function () {
         basic.turnRgbLedOff()
     }
     if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel)) {
+        calliBot2.setLed(C2Motor.beide, false)
         calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 76)
     } else {
         if (calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.hell)) {
+            calliBot2.setLed(C2Motor.links, true)
             calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 65)
             calliBot2.motorStop(C2Motor.links, C2Stop.Bremsen)
         } else {
             if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.hell)) {
+                calliBot2.setLed(C2Motor.rechts, true)
                 calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, 65)
                 calliBot2.motorStop(C2Motor.rechts, C2Stop.Bremsen)
             }
         }
     }
+})
+basic.forever(function () {
+    calliBot2.setRgbLed1(C2RgbLed.RV, 0xff0000, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.LV, 0x0000ff, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.LH, 0x00ff00, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.RH, 0xffff00, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.RV, 0xa300ff, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.LH, 0x00ffdc, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.RV, 0xff0000, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.LV, 0xffff00, 6)
+    basic.pause(100)
+    calliBot2.setRgbLed1(C2RgbLed.RH, 0x00ffdc, 6)
 })
