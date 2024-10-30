bd4328e8e177a1e05bf108b78002bd94e69a935c
Zoellisam <zoellisam@gmail.com>
2024-10-30 16:17:26 +0100

    smiley

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+let zeit = 0
 basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
     basic.setLedColor(0x00ffff)
     if (calliBot2.readBumperSensor(C2Sensor.rechts, C2State.an) || calliBot2.readBumperSensor(C2Sensor.links, C2State.an)) {
         calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
@@ -13,29 +15,35 @@ basic.forever(function () {
         basic.pause(500)
         while (!(input.buttonIsPressed(Button.A))) {
             calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
+            if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel)) {
+                calliBot2.setLed(C2Motor.beide, false)
+                calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 100)
+            } else {
+                if (calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.hell)) {
+                    calliBot2.setLed(C2Motor.links, true)
+                    calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 80)
+                    calliBot2.motorStop(C2Motor.links, C2Stop.Bremsen)
+                } else {
+                    if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.hell)) {
+                        calliBot2.setLed(C2Motor.rechts, true)
+                        calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, 80)
+                        calliBot2.motorStop(C2Motor.rechts, C2Stop.Bremsen)
+                    }
+                }
+            }
         }
     }
     if (calliBot2.entfernung(C2Einheit.cm) < 10) {
         calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
+        zeit += 1
         basic.setLedColor(0xff0000)
+        if (zeit > 4) {
+            zeit = 0
+            basic.showString("mir wird langweilig")
+            basic.showIcon(IconNames.Sad)
+        }
         basic.pause(2000)
         basic.turnRgbLedOff()
-    }
-    if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel)) {
-        calliBot2.setLed(C2Motor.beide, false)
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 100)
-    } else {
-        if (calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.hell)) {
-            calliBot2.setLed(C2Motor.links, true)
-            calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 80)
-            calliBot2.motorStop(C2Motor.links, C2Stop.Bremsen)
-        } else {
-            if (calliBot2.readLineSensor(C2Sensor.rechts, C2SensorStatus.dunkel) && calliBot2.readLineSensor(C2Sensor.links, C2SensorStatus.hell)) {
-                calliBot2.setLed(C2Motor.rechts, true)
-                calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, 80)
-                calliBot2.motorStop(C2Motor.rechts, C2Stop.Bremsen)
-            }
-        }
     }
 })
 basic.forever(function () {
